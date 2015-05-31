0a996793951a271c99fd4a5a57ff1596257fa332
Death259 <Death259@gmail.com>
2015-05-31 01:57:16 -0500

    Reworked and Fixed WiFi
    The connecting to WiFi networks functionality has been reworked and is
    now working properly. It writes to the /etc/network/interfaces file. If
    the Pi gets turned off and back on, it should try to auto connect to the
    wifi network provided.

--- PiAssist.sh
+++ PiAssist.sh
@@ -54,7 +54,7 @@ while true; do
 				--cancel-label "Back" \
 				--menu "Please select:" $HEIGHT $WIDTH 3 \
 				"1" "Display Network Information" \
-				"2" "Scan for WiFI Networks" \
+				"2" "Scan for WiFI Networks (Root Required)" \
 				"3" "Connect to WiFi Network (Root Required)" \
 				2>&1 1>&3)
 			exit_status=$?
@@ -75,32 +75,55 @@ while true; do
 					display_result "Network Information"
 					;;
 				2 )
-					result=$(iwlist wlan0 scan | grep ESSID | sed 's/ESSID://g;s/"//g;s/^ *//;s/ *$//')
-					display_result "WiFi Networks"
+					currentUser=$(whoami)
+					if [ $currentUser == "root" ] ; then
+						ifconfig wlan0 up
+						result=$(iwlist wlan0 scan | grep ESSID | sed 's/ESSID://g;s/"//g;s/^ *//;s/ *$//')
+						display_result "WiFi Networks"
+					else
+						result=$(echo "You have to be running the script as root in order to connect to a WiFi network. Please try using sudo.")
+						display_result "WiFi Network"
+					fi
 					;;
 				3 )
 					currentUser=$(whoami)
 					if [ $currentUser == "root" ] ; then
+						ifconfig wlan0 up
 						wifiNetworkList=$(iwlist wlan0 scan | grep ESSID | sed 's/ESSID://g;s/"//g;s/^ *//;s/ *$//')
 						wifiSSID=$(dialog --title "WiFi Network SSID" --backtitle "Pi Assist" --inputbox "Network List: \n\n$wifiNetworkList \n\nEnter the SSID of the WiFi network you would like to connect to:" 0 0 2>&1 1>&3);
-						if [ $wifiSSID != "" ] ; then
-							wifiPassword=$(dialog --title "WiFi Network Password" --backtitle "Pi Assist" --insecure --passwordbox "Enter the password of the WiFi network you would like to connect to:" 0 0 2>&1 1>&3);
-							turnOnWifiResult=$(ifconfig wlan0 up)
-							connectionResults=$(iwconfig wlan0 essid $wifiSSID key s:$wifiPassword)
-							dhcpResult=$(dhclient wlan0)
-							read -p "Press [Enter] key to start backup..."
-							result=$(echo $turnOnWifiResult )
-							display_result "turnOnWifiResult"
-							result=$(echo $connectionResults )
-							display_result "connectionResults"
-							result=$(echo $dhcpResult )
-							display_result "dhcpResult"
-							if [ $turnOnWifiResult == "" ] && [ $connectionResults == "" ] && [[ $a == "Reloading*" ]] ; then
-								result=$(echo "You are now connected to the $wifiSSID network")
-								display_result "WiFi Network Connection"
+						if [ "$wifiSSID" != "" ] ; then
+							actuallyConnectToWifi=false
+							networkInterfacesConfigLocation="/etc/network/interfaces"
+							response=$(dialog --title "Create Backup?" --inputbox "Would you like to create a backup of your current network interfaces config?" 0 0 2>&1 1>&3)
+							response=${response,,}    # tolower
+							if [[ $response =~ ^(yes|y)$ ]] ; then
+								if [ ! -f $networkInterfacesConfigLocation"_bak" ] ; then
+									cp $networkInterfacesConfigLocation $networkInterfacesConfigLocation"_bak"
+								else
+									backupExistsResponse=$(dialog --title "Overwrite Backup?" --inputbox "A backup currently exists. Do you want to overwrite it?" 0 0 2>&1 1>&3)
+									backupExistsResponse=${backupExistsResponse,,}    # tolower
+									if [[ $backupExistsResponse =~ ^(yes|y)$ ]] ; then
+										cp $networkInterfacesConfigLocation $networkInterfacesConfigLocation"_bak"
+									fi
+									actuallyConnectToWifi=true
+								fi		
 							else
-								result=$(echo "An issue occurred connecting to the WiFi network. ")
-								display_result "WiFi Network Connection"
+								actuallyConnectToWifi=true
+							fi
+							if [ $actuallyConnectToWifi == true ] ; then
+								wifiPassword=$(dialog --title "WiFi Network Password" --backtitle "Pi Assist" --insecure --passwordbox "Enter the password of the WiFi network you would like to connect to:" 0 0 2>&1 1>&3);
+								echo -e 'auto lo\n\niface lo inet loopback\niface eth0 inet dhcp\n\nallow-hotplug wlan0\nauto wlan0\niface wlan0 inet dhcp\n\twpa-ssid "'$wifiSSID'"\n\twpa-psk "'$wifiPassword'"' > $networkInterfacesConfigLocation
+								ifdown wlan0 > /dev/null 2>&1
+								ifup wlan0 > /dev/null 2>&1
+								
+								inetAddress=$(ifconfig wlan0 | grep "inet addr.*")
+								if [ "$inetAddress" != "" ] ; then
+									result=$(echo "You are now connected to $wifiSSID.")
+									display_result "WiFi Network"
+								else
+									result=$(echo "There was an issue trying to connect to $wifiSSID. Please ensure you typed the SSID and password correctly.")
+									display_result "WiFi Network"
+								fi
 							fi
 						fi
 					else
